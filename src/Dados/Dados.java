552a23ceba45e51d69d5dc918180fde27616cbc2
Eder Bragança <eder.braganca@aluno.ufop.edu.br>
2023-08-07 11:39:03 -0300

    Faltou a famosa

--- src/Dados/Dados.java
+++ src/Dados/Dados.java
@@ -6,13 +6,22 @@ import Modelo.Emprestimo;
 import Modelo.Funcionario;
 import Modelo.Livro;
 import Modelo.Usuario;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Dados {
-    public List<Funcionario> listaFuncionarios;
-    public List<Usuario> listaUsuarios;
-    public List<Autor> listaAutores;
-    public List<Livro> listaLivros;
-    public List<Categoria> listaCategorias;
-    public List<Emprestimo> listaEmprestimos;
+    private static Dados dados;
+
+    public static Dados getDados() {
+        if (dados == null) {
+            dados = new Dados();
+        }
+        return dados;
+    }
+    public List<Funcionario> listaFuncionarios = new ArrayList<>();
+    public List<Usuario> listaUsuarios = new ArrayList<>();
+    public List<Autor> listaAutores = new ArrayList<>();
+    public List<Livro> listaLivros = new ArrayList<>();
+    public List<Categoria> listaCategorias = new ArrayList<>();
+    public List<Emprestimo> listaEmprestimos = new ArrayList<>();
 }
